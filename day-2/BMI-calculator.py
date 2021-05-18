eb83ac063ecb9bfe9fa536a769a830a87abeefa7
Herman Ling <hermanling1@gmail.com>
2021-05-18 16:23:35 +0800

    Delete files and move the content to renamed files to resolve order arrangement of files

--- day-2/BMI-calculator.py
+++
@@ -1,5 +0,0 @@
-height = float(input("enter your height in m: "))
-weight = float(input("enter your weight in kg: "))
-
-bmi = round(weight / height ** 2, 2)
-print(bmi)
--- day-3/leap-year-challenge.py
+++
@@ -1,9 +0,0 @@
-year = int(input("Which year do you want to check? "))
-
-if year % 4 == 0:
-	if (year % 100 == 0 and year % 400 == 0) or year % 100 != 0:
-		print("Leap year")
-	else:
-		print("Not leap year")
-else:
-	print("Not leap year")
